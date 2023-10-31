7d23700d2c1d9dd52d3ac2a1eda625af938af820
konstuanotop <konstantin-1997@mail.ru>
2023-10-31 14:07:22 +0500

    first commit

---
+++ html/Feather/js/script.js
@@ -0,0 +1,12 @@
+new Swiper('.reviews-slider', {
+
+    spaceBetween: 40,
+    slidesPerView: 'auto',
+    loop: true,
+    speed: 500,
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnIneraction: false,
+    },
+});
